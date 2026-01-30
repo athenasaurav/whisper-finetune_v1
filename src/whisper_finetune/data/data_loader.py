c2cf1836a3d266f4d1cae4c206466bb6f178de68
Kumar Saurav <athena.saurav@gmail.com>
2026-01-30 13:07:45 +0400

    Removing Prompts in Data Loader

--- src/whisper_finetune/data/data_loader.py
+++ src/whisper_finetune/data/data_loader.py
@@ -171,11 +171,12 @@ class AudioDataset(Dataset):
         return len(self.valid_indices)
 
     def _get_prompt_tokens(self, record: Record, no_timestamps: bool) -> List[int]:
-        if torch.rand(1).item() < self.prompt_use_rate and len(record["prompt"]) > 0:
+        prompt = record.get("prompt", "") or ""
+        if torch.rand(1).item() < self.prompt_use_rate and len(prompt) > 0:
             if no_timestamps:
-                prompt_tokens = self._encode_text_without_timestamps(record["prompt"])[-self.max_prompt_length :]
+                prompt_tokens = self._encode_text_without_timestamps(prompt)[-self.max_prompt_length :]
             else:
-                prompt_tokens = self._encode_text_with_timestamps(record["prompt"])[-self.max_prompt_length :]
+                prompt_tokens = self._encode_text_with_timestamps(prompt)[-self.max_prompt_length :]
             prompt_tokens = [self.tokenizer.sot_prev] + prompt_tokens
         else:
             prompt_tokens = []
